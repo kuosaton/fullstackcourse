4289eb8560bab823206978c1237318035380d2e2
Tony Kuosa <tony.kuosa@outlook.com>
2025-01-16 16:13:22 +0200

    t.1.6 unicafe step1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,50 @@
 import { useState } from 'react'
 
+const Display = props => <div>{props.name} {props.value}</div>
+
+const Header = ( {title} ) => {
+  return (
+    <h2>{title}</h2>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <Header title='Give feedback' />
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+
+      <Header title='Statistics' />
+      <Display name='good' value={good} />
+      <Display name='neutral' value={neutral} />
+      <Display name='bad' value={bad} />
     </div>
   )
 }
