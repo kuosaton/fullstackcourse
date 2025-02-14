fc3616f1e40ae6f6a21dcfb0d7b969b7bc5b918a
Tony Kuosa <tony.kuosa@outlook.com>
2025-02-14 14:40:38 +0200

    tehtävä 3.14

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -2,6 +2,13 @@ const express = require("express")
 const morgan = require("morgan")
 const app = express()
 
+require('dotenv').config()
+
+const Person = require('./models/person')
+
+let persons = [
+]
+
 app.use(express.json())
 app.use(morgan("tiny"))
 
@@ -10,35 +17,18 @@ app.use(cors())
 
 app.use(express.static("dist"))
 
-let persons = [
-   {
-      id: "1",
-      name: "Arto Hellas",
-      number: "040-123456",
-   },
-   {
-      id: "2",
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-   },
-   {
-      id: "3",
-      name: "Dan Abramov",
-      number: "12-43-234345",
-   },
-   {
-      id: "4",
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-   },
-]
+const unknownEndpoint = (request, response) => {
+   response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get("/", (request, response) => {
    response.send("<h1>Hello World!</h1>")
 })
 
 app.get("/api/persons", (request, response) => {
-   response.json(persons)
+   Person.find({}).then(persons => {
+      response.json(persons)
+   })
 })
 
 app.get("/info", (request, response) => {
@@ -74,11 +64,12 @@ const generateId = () => {
 
 app.post("/api/persons", (request, response) => {
    const body = request.body
-   if (!body.name) {
+
+   if (body.name === undefined) {
       return response.status(400).json({
          error: "Name missing",
       })
-   } else if (!body.number) {
+   } else if (body.number === undefined) {
       return response.status(400).json({
          error: "Number missing",
       })
@@ -88,18 +79,20 @@ app.post("/api/persons", (request, response) => {
       })
    }
 
-   const person = {
+   const person = new Person({
       id: generateId(),
       name: body.name,
       number: body.number,
-   }
+   })
 
-   persons = persons.concat(person)
-
-   response.json(person)
+   person.save().then(savedPerson => {
+      response.json(savedPerson)
+   })
 })
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`)
 })
