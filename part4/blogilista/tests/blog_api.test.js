3c1ee22376ed25aacc1ecd1303276c71daa268cf
Tony Kuosa <120479105+kuosaton@users.noreply.github.com>
2025-12-19 21:11:58 +0200

    tehtävä 4.13

--- part4/blogilista/tests/blog_api.test.js
+++ part4/blogilista/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, afterEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -8,94 +8,109 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
-})
-
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
-
-test('blogs are returned with id as the identifying field', async () => {
-  const response = await api.get('/api/blogs')
-  response.body.forEach((blog) => {
-    assert.ok(blog.id)
-    assert.strictEqual(blog._id, undefined)
+describe('when there are initially blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
   })
-})
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'Physics 2',
-    author: 'Albert Einstein',
-    url: 'example.com/albert-new-cool-paper-abstract.html',
-    likes: 267,
-  }
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
 
-  const title = blogsAtEnd.map((blog) => blog.title)
-  assert(title.includes('Physics 2'))
-})
+  test('blogs are returned with id as the identifying field', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach((blog) => {
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
+    })
+  })
 
-after(async () => {
-  await mongoose.connection.close()
-})
+  describe('adding a new blog', () => {
+    test('succeeds with valid inputs', async () => {
+      const newBlog = {
+        title: 'Physics 2',
+        author: 'Albert Einstein',
+        url: 'example.com/albert-new-cool-paper-abstract.html',
+        likes: 267,
+      }
 
-test('adding a blog without specifying likes defaults to 0', async () => {
-  const newBlog = {
-    title: 'Physics 2',
-    author: 'Albert Einstein',
-    url: 'example.com/albert-new-cool-paper-abstract.html',
-  }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+      const title = blogsAtEnd.map((blog) => blog.title)
+      assert(title.includes('Physics 2'))
+    })
 
-  const likes = blogsAtEnd.map((blog) => blog.likes)
-  console.log(typeof likes)
-  assert.strictEqual(likes.at(-1), 0)
-})
+    test('succeeds without specifying likes by defaulting to 0', async () => {
+      const newBlog = {
+        title: 'Physics 2',
+        author: 'Albert Einstein',
+        url: 'example.com/albert-new-cool-paper-abstract.html',
+      }
 
-test('adding a blog without specifying title returns code 400', async () => {
-  const newBlog = {
-    author: 'Albert Einstein',
-    url: 'example.com/albert-new-cool-paper-abstract.html',
-  }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  await api.post('/api/blogs').send(newBlog).expect(400)
-})
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-test('adding a blog without specifying url returns code 400', async () => {
-  const newBlog = {
-    title: 'Physics 2',
-    author: 'Albert Einstein',
-  }
+      const likes = blogsAtEnd.map((blog) => blog.likes)
+      console.log(typeof likes)
+      assert.strictEqual(likes.at(-1), 0)
+    })
 
-  await api.post('/api/blogs').send(newBlog).expect(400)
+    test('fails with code 400 if title is not specified', async () => {
+      const newBlog = {
+        author: 'Albert Einstein',
+        url: 'example.com/albert-new-cool-paper-abstract.html',
+      }
+
+      await api.post('/api/blogs').send(newBlog).expect(400)
+    })
+
+    test('fails with code 400 if url is not specified', async () => {
+      const newBlog = {
+        title: 'Physics 2',
+        author: 'Albert Einstein',
+      }
+
+      await api.post('/api/blogs').send(newBlog).expect(400)
+    })
+  })
+
+  describe('deleting a blog', () => {
+    test('succeeds with code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      const ids = blogsAtEnd.map((blog) => blog.id)
+      assert(!ids.includes(blogToDelete.id))
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+    })
+  })
 })
 
 after(async () => {
