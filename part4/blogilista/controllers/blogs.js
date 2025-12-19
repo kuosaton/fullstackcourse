d9639c3b931748e67fccae76c89d5658508d5b76
Tony Kuosa <120479105+kuosaton@users.noreply.github.com>
2025-12-19 23:45:48 +0200

    tehtävät 4.17, 4.18

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,8 +1,10 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -19,13 +21,34 @@ blogsRouter.get('/:id', async (request, response, next) => {
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   try {
     const { title, author, url, likes } = request.body
 
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+      return response.status(400).json({ error: 'UserId missing or not valid' })
+    }
     if (!title) {
       return response.status(400).json({ error: 'title is required' })
-    } else if (!url) {
+    }
+    if (!author) {
+      return response.status(400).json({ error: 'author is required' })
+    }
+    if (!url) {
       return response.status(400).json({ error: 'url is required' })
     }
 
@@ -34,9 +57,13 @@ blogsRouter.post('/', async (request, response, next) => {
       author: author,
       url: url,
       likes: likes || 0,
+      user: user._id,
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
   } catch (exception) {
     next(exception)
