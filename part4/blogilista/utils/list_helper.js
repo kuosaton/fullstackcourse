ff42363f657f6e9f0ce28466f957591c48bbcccd
Tony Kuosa <120479105+kuosaton@users.noreply.github.com>
2025-12-19 17:09:50 +0200

    tehtävä 4.4

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -1,7 +1,10 @@
-const dummy = (blogs) => {
-  return 1
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, currentValue) => {
+    return accumulator + currentValue.likes
+  }
+  return blogs.reduce(reducer, 0)
 }
 
 module.exports = {
-  dummy,
+  totalLikes,
 }
