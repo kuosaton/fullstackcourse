4c9aa6c901e6667c0abd3167af1ea2829fcbe359
Tony Kuosa <120479105+kuosaton@users.noreply.github.com>
2025-12-19 19:51:20 +0200

    tehtävä 4.8

--- part4/blogilista/utils/config.js
+++ part4/blogilista/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
-const MONGODB_URI_PUBLIC = process.env.MONGODB_URI_PUBLIC // MongoDB URI with password redacted for console logging
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
-module.exports = { MONGODB_URI, PORT, MONGODB_URI_PUBLIC }
+module.exports = { MONGODB_URI, PORT }
