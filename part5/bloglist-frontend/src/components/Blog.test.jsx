210d57adfc683bcb359ea5841c9221b66e32fd3c
Tony Kuosa <120479105+kuosaton@users.noreply.github.com>
2025-12-20 18:45:28 +0200

    tehtävä 5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -36,3 +36,33 @@ test('blog displays author, url, and likes when expanded', async () => {
   expect(screen.getByText(blog.url)).toBeVisible()
   expect(screen.getByText(`likes ${blog.likes}`)).toBeVisible()
 })
+
+test('pressing like twice calls event handler twice', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'joulupukki',
+    url: 'example.com',
+    likes: 246,
+    user: { username: 'user', name: 'Testi Käyttäjä', id: '123' },
+  }
+
+  const mockLikeHandler = vi.fn()
+  const user = userEvent.setup()
+
+  render(
+    <Blog
+      blog={blog}
+      user={{ id: '123' }}
+      onLike={mockLikeHandler}
+      onDelete={() => {}}
+    />
+  )
+
+  await user.click(screen.getByText('view'))
+
+  const likeButton = screen.getByRole('button', { name: 'like' })
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockLikeHandler).toHaveBeenCalledTimes(2)
+})
