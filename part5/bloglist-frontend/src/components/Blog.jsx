cf8a94e658c89bf608d6d4777aa14ad54a3e3295
Tony Kuosa <120479105+kuosaton@users.noreply.github.com>
2025-12-20 17:11:56 +0200

    tehtävä 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,11 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    <div>
-      <strong>{blog.title}</strong> <span>by {blog.author}</span>
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} <span>by {blog.author}</span>{' '}
+        <button onClick={() => setVisible(true)}>view</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        <div>
+          <strong>{blog.title}</strong> <span>by {blog.author}</span>{' '}
+          <button onClick={() => setVisible(false)}>hide</button>
+        </div>
+
+        <div>
+          {blog.likes} likes <button type="button">like</button>
+        </div>
+        <div>available at: {blog.url}</div>
+      </div>
     </div>
-    <div>{blog.likes} likes</div>
-    <div>available at: {blog.url}</div>
-  </div>
-)
+  )
+}
 
 export default Blog
